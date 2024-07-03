998f45ee848b6c20300b786502dacf1157f76d97
Glitch (glitch-hello-node) <none>
2024-07-03 14:19:56 +0000

    🏨🌴 Checkpoint ./server.js:68907828/575

--- server.js
+++ server.js
@@ -7,24 +7,31 @@ app.use(cors());
 const port = 3000;
 const url = "https://goldenrod-verdant-rayon.glitch.me"
 let log = [];
-
+const secretpath = '/l'
+const cupsfile = '/app/cups.html'
 app.use(express.json());
 
 // Endpoint to receive button press logs
 app.post('/log', (req, res) => {
   const dugme = req.body
-  log.push({dugme, timestamp: new Date() });
-  //salji poruku for x in wslist. oni treba da dodaju dom element na dno
+  const newclick = {dugme, timestamp: new Date() };
+  log.push(newclick);
+  //salji poruku sa newclick svim konekcijama u watchers. oni treba da dodaju dom element na dno
   res.sendStatus(200);
 });
 
 // Endpoint to retrieve the log (for demonstration purposes)
-app.get('/log', (req, res) => {
-  //ovde bi mogao da se doda websocket u globalnu promenljivu wslist
+app.get(secretpath, (req, res) => {
+  //ovde bi mogao da se doda websocket u globalnu promenljivu watchers
+  //umesto da se salje odmah log, trb da se salje prazna stranica pa odmah svaki element loga dotad preko websocketa.
   res.json(log);
 });
 app.get('/',(req,res) =>{
-  res.sendFile('/app/cups.html');
+  res.sendFile(cupsfile);
+});
+app.post('/resetlog',(req,res) =>{
+  log=[]
+  res.sendStatus(200);
 });
 app.listen(port, () => {
   console.log(`Server listening at ${url}:${port}`);
